125d93063b360ba716934d0af09f6743a6351c8c
koba <koba@growal.com>
2022-06-04 09:28:11 +0900

    Initial Commit

---
+++ SwiftStartupKit/SwiftStartupKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStartupKitApp.swift
+//  SwiftStartupKit
+//
+//  Created by Masahiko Kobayashi on 2022/06/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStartupKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
