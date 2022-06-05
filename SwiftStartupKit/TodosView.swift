9e778487e365d5ec1fbe506b6990301da3bb3dcc
koba <koba@growal.com>
2022-06-05 16:32:26 +0900

    feat: todos view

---
+++ SwiftStartupKit/TodosView.swift
@@ -0,0 +1,20 @@
+//
+//  TodosView.swift
+//  SwiftStartupKit
+//
+//  Created by Masahiko Kobayashi on 2022/06/05.
+//
+
+import SwiftUI
+
+struct TodosView: View {
+    var body: some View {
+        Text("Todos View")
+    }
+}
+
+struct TodosView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodosView()
+    }
+}
