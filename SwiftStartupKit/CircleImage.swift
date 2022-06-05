387bfe2d829fb39f689ea1af7912543fefd04a3e
koba <koba@growal.com>
2022-06-05 09:36:37 +0900

    circle

---
+++ SwiftStartupKit/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SwiftStartupKit
+//
+//  Created by Masahiko Kobayashi on 2022/06/05.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(.white, lineWidth: 4)
+            )
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
