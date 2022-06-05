e5bc669a3ae607d6fa427976d61646dba2d44db2
koba <koba@growal.com>
2022-06-05 10:53:46 +0900

    m

---
+++ SwiftStartupKit/MapView.swift
@@ -0,0 +1,20 @@
+//
+//  MapView.swift
+//  SwiftStartupKit
+//
+//  Created by Masahiko Kobayashi on 2022/06/05.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
