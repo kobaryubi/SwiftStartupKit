ff1470e17ab430c0532b6c206840b38b534958da
koba <koba@growal.com>
2022-06-23 22:40:06 +0900

    feat: content view

--- SwiftStartupKit/ContentView.swift
+++ SwiftStartupKit/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Todos View")
-            .navigationTitle("Todos")
+        NavigationLink(destination: TodosView()) {
+            Label("Todos", systemImage: "list.bullet")
+        }
+        .navigationTitle("Menu")
     }
 }
 
